9705b1af576b5460a061d33c9e7587ad5e52f963
rensramos <rensramos17@gmail.com>
2016-07-06 18:37:44 +0800

    add initial slider

---
+++ _sliderItem/process.md
@@ -0,0 +1,11 @@
+---
+rank: 3
+title: "Process"
+description: Lorem ipsum dolor sit amet.
+content:
+    - title: "Alkaline"
+      description: "An alkaline is any substance with a pH rating lower than 7; that is, any substance with fewer hydrogen ions than pure water. This is opposed to an acid, which is any substance with more hydrogen ions than water. The further the pH is from 7 the more of an acid or alkaline the substance is, depending on which way you're going."
+
+    - title: "Purified"
+      description: "The purest water out there, purified water refers to the quality of water. There a number of different ways it can reach this quality, including distillation, reverse osmosis, ion exchange and more. To be classified as purified, the water must have less than 10 PPM."
+---
---
+++ js/home.js
@@ -0,0 +1,70 @@
+var bsi = bsi || {};
+bsi.init = function() {
+    bsi.initImgSlider();
+};
+
+
+bsi.initImgSlider = function() {
+    var owl = $('.slider').owlCarousel({
+        lazyLoad: true,
+        autoPlay: false,
+        mouseDrag:true,
+        touchDrag: true,
+        video: true,
+        items:1,
+        navText: [
+            '<img class="img-responsive" src="/img/arrow/arrow-left-grey.png"/>',
+            '<img class="img-responsive" src="/img/arrow/arrow-right-grey.png"/>'
+        ],
+        smartSpeed: 1000,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                nav: false,
+                video: true
+
+            },
+            // breakpoint from 480 up
+            992 : {
+                nav: true,
+                video: true
+
+            }
+        }
+    });
+    owl.on('changed.owl.carousel',function(property){
+        var current = property.item.index;
+        var rank = $(property.target).find(".owl-item").eq(current).find('li').data('rank');
+        var color = $(property.target).find(".owl-item").eq(current).find('li').data('color');
+        var description = $(property.target).find(".owl-item").eq(current).find('li').data('description');
+
+        $("#video-01")[0].pause();
+        $("#video-02")[0].pause();
+        $("#video-03")[0].pause();
+
+        $("#bg-color").css("background-color",color);
+        $(".modal-content").css("background-color",color);
+
+        $(".section-description").fadeOut(function() {
+            $(this).html(description).fadeIn();
+        });
+        $(".modal-text").fadeOut(function() {
+            $(this).html(description).fadeIn();
+        });
+
+
+    });
+
+};
+
+
+
+$(function(){
+    bsi.init();
+});
+
+
+
+
+
+
